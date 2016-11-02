cbab45c044e2126fb41fde967152fed2cb1a3301
Laura González <lawr@lauragonzalez.cc>
2016-11-02 22:34:43 +0100

    expose env

---
+++ src/env.js
@@ -0,0 +1,8 @@
+module.exports = {
+	clientId: process.env.PHOTO_CID,
+	clientSecret: process.env.PHOTO_CS,
+	twitterConsumerKey: process.env.TWITTER_APPK,
+	twitterConsumerSecret: process.env.TWITTER_APPS,
+	twitterAccess: process.env.TWITTER_AT,
+	twitterSecret: process.env.TWITTER_S
+}
